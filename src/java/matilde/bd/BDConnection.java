78b8d54743c0328dd9beefe415dc879fd7f38820
sebastian-reyes <sebastian_reyes_2105@hotmail.com>
2021-01-26 13:12:25 -0500

    Conexion a Base de datos

---
+++ src/java/matilde/bd/BDConnection.java
@@ -0,0 +1,32 @@
+
+package matilde.bd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class BDConnection {
+    private String bd="bdpasteleria";
+    private String url="jdbc:mysql://localhost:3306/"+bd;
+    private String user="root";
+    private String password="12345";
+    Connection conexion = null;
+    
+    public Connection Conectar(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");          
+            conexion = (Connection)DriverManager.getConnection(url, user, password);
+        }catch(ClassNotFoundException|SQLException ex){
+            System.out.println(ex.getMessage());
+        }
+        return conexion;
+    }
+    
+    public void Desconectar(){
+        try{
+            conexion.close();
+        }catch(SQLException ex){
+            System.out.println("Error al cerrar conexión");
+        }
+    }
+}
