a6a70a96618bada68bb38d2366172f6ced889557
sebastian-reyes <sebastian_reyes_2105@hotmail.com>
2021-01-26 14:50:57 -0500

    Agregando IDao + Dao de Categoria

---
+++ src/java/matilde/idao/I_CategoriaDao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matilde.idao;
+
+import java.util.List;
+import matilde.model.Categoria;
+
+/**
+ *
+ * @author Sebastián
+ */
+public interface I_CategoriaDao {
+    public List<Categoria> listarCat();
+}
